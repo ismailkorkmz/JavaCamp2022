077710af1b270f462f80447540db1da86cc26c25
hasankorogluismailkorkmz <hilal_ay_1907@windowslive.com>
2022-09-21 20:47:24 +0300

    Hafta 1

--- arrayDemo/src/arrayDemo/Main.java
+++ arrayDemo/src/arrayDemo/Main.java
@@ -3,11 +3,12 @@ package arrayDemo;
 public class Main {
 
 	public static void main(String[] args) {
-		String[] ogrenciler = new String[4];
+		String[] ogrenciler = new String[5];
 		ogrenciler[0]="Engin";
 		ogrenciler[1]="İsmail";
 		ogrenciler[2]="Eda";
 		ogrenciler[3]="Gizem";
+		ogrenciler[4]="Elif";
 		
 		for (int i = 0; i < ogrenciler.length; i++) {
 			System.out.println(ogrenciler[i]);
